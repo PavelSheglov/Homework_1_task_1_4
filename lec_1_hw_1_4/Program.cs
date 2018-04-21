a1019d14c7eba46f73b963e2b1eb8f9772b93517
PavelSheglov <tpaul-1981@yandex.ru>
2018-04-21 16:29:42 +0700

    Разбивка на функции

--- lec_1_hw_1_4/Program.cs
+++ lec_1_hw_1_4/Program.cs
@@ -10,48 +10,74 @@ namespace lec_1_hw_1_4
     {
         static void Main(string[] args)
         {
-            try
+            int N=InputSizeOfSequence();
+            short[] sequence = InputSequence(N);
+            CalculateAndShowSequenceParameters(sequence);
+        }
+
+        static int InputSizeOfSequence()
+        {
+            int size = 0;
+            Console.Clear();
+            do
             {
-                int N;
-                do
+                try
                 {
                     Console.Write("Введите число элементов (для ввода):");
-                    N = Convert.ToInt32(Console.ReadLine());
-                } while (N <= 0);
-                short[] arr = new short[N];
-                Console.WriteLine("Введите последовательность целых чисел (от {0} до {1})", short.MinValue, short.MaxValue);
-                for (int j = 0; j < N; j++)
+                    size = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (FormatException)
                 {
-                    long temp;
-                    do
+                    Console.WriteLine("Ошибка при вводе числа!!!");
+                }
+            } while (size <= 0);
+            return size;
+        }
+        
+        static short[] InputSequence(int size)
+        {
+            short[] sequence = new short[size];
+            Console.Clear();
+            Console.WriteLine("Введите последовательность целых чисел (от {0} до {1})", short.MinValue, short.MaxValue);
+            for (int j = 0; j < size; j++)
+            {
+                long temp=long.MinValue;
+                do
+                {
+                    try
                     {
                         Console.Write("Введите {0}-е число:", j + 1);
                         temp = Convert.ToInt64(Console.ReadLine());
-                    } while (temp < short.MinValue || temp > short.MaxValue);
-                    arr[j] = Convert.ToInt16(temp.ToString());
-                }
-                int sum = 0;
-                short min = short.MaxValue;
-                short max = short.MinValue;
-                int cnt = 0;
-                double prod = 1;
-                int cntodd = 0;
-                foreach (short i in arr)
-                {
-                    sum += i;
-                    if (i > max) max = i;
-                    if (i < min) min = i;
-                    if (i % 2 == 0) cnt++;
-                    else { prod *= i; cntodd++; }
-                }
-                Console.WriteLine("Сумма чисел={0}, максимум={1}, минимум={2}", sum, max, min);
-                Console.WriteLine($"Количество четных чисел={cnt}");
-                Console.WriteLine("Произведение нечетных чисел={0:e}", cntodd > 0 ? prod : 0);
+                    }
+                    catch (FormatException)
+                    {
+                        Console.WriteLine("Ошибка при вводе числа!!!");
+                    }
+                } while (temp < short.MinValue || temp > short.MaxValue);
+                sequence[j] = Convert.ToInt16(temp.ToString());
             }
-            catch(FormatException)
+            return sequence;
+        }
+
+        static void CalculateAndShowSequenceParameters(short[] sequence)
+        {
+            int sumOfValues = 0;
+            short minValue = short.MaxValue;
+            short maxValue = short.MinValue;
+            int countOfEvens = 0;
+            double productOfOdds = 1;
+            int countOfOdds = 0;
+            foreach (short i in sequence)
             {
-                Console.WriteLine("Ошибки при вводе чисел!!!");
+                sumOfValues += i;
+                if (i > maxValue) maxValue = i;
+                if (i < minValue) minValue = i;
+                if (i % 2 == 0) countOfEvens++;
+                else { productOfOdds *= i; countOfOdds++; }
             }
+            Console.WriteLine("Сумма чисел={0}, максимум={1}, минимум={2}", sumOfValues, maxValue, minValue);
+            Console.WriteLine($"Количество четных чисел={countOfEvens}");
+            Console.WriteLine("Произведение нечетных чисел={0:e}", countOfOdds > 0 ? productOfOdds : 0);
         }
     }
 }
